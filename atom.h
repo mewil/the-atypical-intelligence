e4dcb0376130f5703192e21398158b720d16664b
Michael Wilson <Michael@Athena.local>
2017-04-05 17:42:24 -0400

    Added atom.h and molecule.h

---
+++ atom.h
@@ -0,0 +1,24 @@
+#ifndef ATOM_H
+#define ATOM_H
+/* Atom.h
+ *
+ * Abstract data type representing an atom
+ *
+ * By Michael Wilson <mewil@umich.edu>
+ * Updated
+ *   4-5-2017
+ */
+
+
+class Atom {
+
+
+public:
+
+
+
+private:
+
+
+};
+#endif
---
+++ molecule.h
@@ -0,0 +1,24 @@
+#ifndef MOLECULE_H
+#define MOLECULE_H
+/* Molecule.h
+ *
+ * Abstract data type representing an molecule
+ *
+ * By Michael Wilson <mewil@umich.edu>
+ * Updated
+ *   4-5-2017
+ */
+
+
+class Molecule {
+
+
+public:
+
+
+
+private:
+
+
+};
+#endif
